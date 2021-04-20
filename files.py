5abc9cf987c91d262ab3f33256b55d71afd909ed
Alexhex <newbon@gmail.com>
2021-04-20 22:41:55 +0300

    first function done

--- files.py
+++ files.py
@@ -1,6 +1,7 @@
 # -*-coding:utf-8-*-
 
 import os
+import re
 
 # 1. Ask for address of dir A;
 # 2. Ask for address of dir B;
@@ -10,11 +11,17 @@ import os
 # 6. If confirmed, start copying the files;
 # 7. List the changes to a txt file for check;
 
-dest_folder = raw_input("请输入准备寄送的文件夹")
+dest_folder = input("请输入准备寄送的文件夹")
 
-res_folder = raw_input("请输入含有所需文件的文件夹")
+res_folder = input("请输入含有所需文件的文件夹")
 
 
 def list_pdf_files(address_a):
-    with os.scandir(dest_folder) as entries:
-        for entry in entries:
+    for rootpath, dirs, files in os.walk(address_a):
+        for name in files:
+            match_obj = re.search(r"py$", name)
+            if match_obj:
+                print(os.path.join(rootpath, name))
+
+
+list_pdf_files(dest_folder)
